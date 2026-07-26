5c006d3c19248f133973d6772100b47bb2daa9a8
Maks <maxsouldrake@gmail.com>
2026-07-26 12:58:43 +0200

    add correlation id logging

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -1,5 +1,17 @@
-import axios from "axios";
+import axios, {AxiosHeaders, type AxiosInstance} from "axios";
+
+export const CORRELATION_ID_HEADER = "X-Correlation-Id";
 
 export const apiClient = axios.create({baseURL: import.meta.env.VITE_BACKEND_API_URL});
+addCorrelationIdInterceptor(apiClient);
 
 export const mediaClient = axios.create({baseURL: import.meta.env.VITE_MEDIA_API_URL});
+addCorrelationIdInterceptor(mediaClient);
+
+export function addCorrelationIdInterceptor(client: AxiosInstance) {
+    client.interceptors.request.use((config) => {
+        config.headers = AxiosHeaders.from(config.headers);
+        config.headers.set(CORRELATION_ID_HEADER, crypto.randomUUID());
+        return config;
+    });
+}
