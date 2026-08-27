70b7b1f476569f882b58c0dd372df4a9a896ae4e
Maks <maxsouldrake@gmail.com>
2026-08-27 17:58:57 +0200

    create PageHeader component

---
+++ src/shared/components/PageHeader.tsx
@@ -0,0 +1,27 @@
+import type {ReactNode} from "react";
+
+type Props = {
+    title: ReactNode;
+    meta?: ReactNode;
+    controls?: ReactNode;
+    children?: ReactNode;
+};
+
+export function PageHeader({title, meta, controls, children}: Readonly<Props>) {
+    return (
+        <div className="page-title">
+            <h1>{title}</h1>
+            {children && (
+                <section>
+                    {children}
+                </section>
+            )}
+            <div>
+                <div>{meta}</div>
+                <div className="page-title-controls">
+                    {controls}
+                </div>
+            </div>
+        </div>
+    );
+}
